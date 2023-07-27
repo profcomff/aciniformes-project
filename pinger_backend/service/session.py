03a99b18fe312b81b3ec4acc880707477dcc4c4b
semen603089 <semen603089@mail.ru>
2023-07-27 19:52:51 +0300

    fix session

--- pinger_backend/service/session.py
+++ pinger_backend/service/session.py
@@ -1,15 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, declarative_base, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
-from settings import get_settings as db_settings
+from settings import get_settings
+
+
+settings = get_settings()
+
+engine = create_engine(str(settings.DB_DSN), execution_options={"isolation_level": "AUTOCOMMIT", "pool_pre_ping": True})
+session = sessionmaker(bind=engine)
 
 
 def dbsession() -> Session:
-    settings = db_settings()
-    engine = create_engine(str(settings.DB_DSN), execution_options={"isolation_level": "AUTOCOMMIT"})
-    session = sessionmaker(bind=engine)
-    Base = declarative_base()
+    global session
     localsession = session()
-    Base.metadata.create_all(engine)
 
     return localsession
