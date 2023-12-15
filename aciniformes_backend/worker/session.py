3c99b5716b244206bb3df3a247f1c83f8cb6c577
semen603089 <semen603089@mail.ru>
2023-12-15 11:21:56 +0300

    fix session

--- aciniformes_backend/worker/session.py
+++ aciniformes_backend/worker/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from aciniformes_backend.settings import get_settings
 
@@ -7,11 +7,4 @@ from aciniformes_backend.settings import get_settings
 settings = get_settings()
 
 engine = create_engine(str(settings.DB_DSN), execution_options={"isolation_level": "AUTOCOMMIT", "pool_pre_ping": True})
-session = sessionmaker(bind=engine)
-
-
-def dbsession() -> Session:
-    global session
-    localsession = session()
-
-    return localsession
+session_factory = sessionmaker(bind=engine)
